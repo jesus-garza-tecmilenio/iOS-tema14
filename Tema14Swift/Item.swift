effaf80f9ae8f225c0f99c16fe48d80a9b337da2
Jesus Garza <jesus.garza.gro@hotmail.com>
2025-11-19 23:21:34 -0600

    Initial Commit

---
+++ Tema14Swift/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Tema14Swift
+//
+//  Created by Jesus Alberto Garza Guerrero on 19/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
